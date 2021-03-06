1a356046565d345f0a9b5b644cf664ab124aae3d
sunrise <adrienmarcotte@gmail.com>
2021-03-06 11:39:08 -0500

    Conversation et message incomplet

--- devoir3/src/src/Message.java
+++ devoir3/src/src/Message.java
@@ -1,5 +1,19 @@
 package src;
 
-public class Message {
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Message implements Serializable {
+    private Etudiant envoyeur;
+    private String message;
+    private Timestamp dateEnvoie;
+
+    public Message(Etudiant envoyeur, String message, Timestamp dateEnvoie) {
+        this.envoyeur = envoyeur;
+        this.message = message;
+        this.dateEnvoie = dateEnvoie;
+    }
+
 
 }
+
