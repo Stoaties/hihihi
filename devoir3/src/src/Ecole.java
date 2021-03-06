6cad6b6c0f3707c7601464cb147ecd84cd15887f
Stoat <35821119+tourtiere42@users.noreply.github.com>
2021-03-06 11:39:06 -0500

    Etudiant supp

--- devoir3/src/src/Ecole.java
+++ devoir3/src/src/Ecole.java
@@ -1,5 +1,31 @@
 package src;
 
-public class Ecole {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Ecole implements Serializable{
+	private String nom;
+	
+	private List<Etudiant> etudiants;
+	private List<Programme> programmes;
+	private List<Cours> cours;
+
+	public Ecole(String nom) {
+		this.nom = nom;
+		etudiants = new ArrayList<Etudiant>();
+		programmes = new ArrayList<Programme>();
+		cours = new ArrayList<Cours>();
+	}
+	
+	public Ecole() {
+		nom = "";
+		etudiants = new ArrayList<Etudiant>();
+		programmes = new ArrayList<Programme>();
+		cours = new ArrayList<Cours>();
+	}
+	
+	public void removeEtudiant(Etudiant etudiant) {
+		etudiants.remove(etudiants);
+	}
 }
--- devoir3/src/src/Etudiant.java
+++ devoir3/src/src/Etudiant.java
@@ -1,5 +1,110 @@
 package src;
 
-public class Etudiant {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Etudiant implements Serializable{
+	private String nom;
+	private String prenom;
+	private Ecole ecole;
+	private List<Etudiant> amis;
+	private List<Conversation> convos;
+	private List<Cours> cours;
+	//private List<Groupe> groupes;
+	private List<String> interets;
+	private String email;
+	private static final Etudiant ETUDIANT_DELETED= new Etudiant("deleted"," user ");
+	
+	public Etudiant(String nom, String prenom) {
+		this.nom = nom;
+		this.prenom = prenom;
+		ecole = new Ecole();
+		amis = new  ArrayList<Etudiant>();
+		convos = new  ArrayList<Conversation>();
+		cours = new  ArrayList<Cours>();
+		interets = new  ArrayList<String>();
+		email = "";
+	}
+	
+	public Etudiant() {
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Ecole getEcole() {
+		return ecole;
+	}
+
+	public void setEcole(Ecole ecole) {
+		this.ecole = ecole;
+	}
+
+	public List<Etudiant> getAmis() {
+		return amis;
+	}
+
+	public void setAmis(List<Etudiant> amis) {
+		this.amis = amis;
+	}
+
+	public List<Conversation> getConvos() {
+		return convos;
+	}
+
+	public void setConvos(List<Conversation> convos) {
+		this.convos = convos;
+	}
+
+	public List<Cours> getCours() {
+		return cours;
+	}
+
+	public void setCours(List<Cours> cours) {
+		this.cours = cours;
+	}
+
+	public List<String> getInterets() {
+		return interets;
+	}
+
+	public void setInterets(List<String> interets) {
+		this.interets = interets;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	////////////////////////////////////////////////////////////////
+	
+	public void addAmi(Etudiant etudiant) {
+		if(!amis.contains(etudiant)) {
+			amis.add(etudiant);
+		}
+	}
+	
+	public void removeAmi(Etudiant etudiant) {
+		amis.remove(etudiant);
+	}
+	
+
 
 }
